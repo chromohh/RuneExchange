e10a0de78a531b5dbef112baea2c1aa844da5269
chromohh <sindre.he@hotmail.com>
2020-06-17 02:24:59 +0200

    db

--- app/src/main/java/com/example/runeexchange/LocalData/DBTools.java
+++ app/src/main/java/com/example/runeexchange/LocalData/DBTools.java
@@ -1,4 +1,36 @@
 package com.example.runeexchange.LocalData;
 
-public class DBTools {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBTools extends SQLiteOpenHelper {
+
+
+    private static final String ITEM_TABLE = "ITEM_TABLE";
+    private static final String COLUMN_ITEM_ID = "id";
+    private static final String COLUMN_ITEM_NAME = "name";
+    private static final String COLUMN_ITEM_CURRENT_PRICE = "currentPrice";
+    private static final String COlUMN_ITEM_PRICE_CHANGE = "priceChange";
+    private static final String ITEM_DONE = "done";
+
+    public DBTools(@Nullable Context context){
+        super(context, "RuneExchange.db", null, 1);
+    }
+
+    public void getItemFromDB(){
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
