5a0992762cb9e798ade892d558cd3fca572215eb
chromohh <sindre.he@hotmail.com>
2020-06-18 11:22:58 +0200

    singleton added

--- app/src/main/java/com/example/runeexchange/LocalData/DBTools.java
+++ app/src/main/java/com/example/runeexchange/LocalData/DBTools.java
@@ -10,11 +10,10 @@ public class DBTools extends SQLiteOpenHelper {
 
 
     private static final String ITEM_TABLE = "ITEM_TABLE";
-    private static final String COLUMN_ITEM_ID = "id";
-    private static final String COLUMN_ITEM_NAME = "name";
-    private static final String COLUMN_ITEM_CURRENT_PRICE = "currentPrice";
-    private static final String COlUMN_ITEM_PRICE_CHANGE = "priceChange";
-    private static final String ITEM_DONE = "done";
+    private static final String COLUMN_ID = "ID";
+    private static final String COLUMN_ITEM_ID = "ITEM_ID";
+    private static final String COLUMN_ITEM_NAME = "NAME";
+    private static final String COLUMN_ITEM_CURRENT_PRICE = "CURRENT_PRICE";
 
     public DBTools(@Nullable Context context){
         super(context, "RuneExchange.db", null, 1);
@@ -24,9 +23,14 @@ public class DBTools extends SQLiteOpenHelper {
 
     }
 
+    public void updatePrice(){
+
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String create_table_query = "CREATE TABLE " + ITEM_TABLE + " (" + COLUMN_ID  + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_ITEM_ID + " INTEGER, " + COLUMN_ITEM_NAME + " TEXT, " + COLUMN_ITEM_CURRENT_PRICE + " INTEGER)";
+        db.execSQL(create_table_query);
     }
 
     @Override
